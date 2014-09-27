9db722df3ceb74f85412cf201aaff9164f83f77f
kazuhito-m <kazuhito.sumpic@gmail.com>
2014-09-28 02:12:55 +0900

    Added new dockerfiles for DB and Java Web Application base image.

---
+++ db_and_webapp/build_and_start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+IMAGE_NAME='centos:db_and_webapp'
+
+CONTAINER_NO='3'
+CONTAINER_NAME="db_and_webapp_${CONTAINER_NO}"
+
+# image build
+docker build -t centos:db_and_webapp .
+
+# container run
+docker run -d --name ${CONTAINER_NAME} -i -p ${CONTAINER_NO}0022:22 -p ${CONTAINER_NO}8080:8080 -p ${CONTAINER_NO}3306:3306 -t ${IMAGE_NAME}
---
+++ db_and_webapp/scripts/startup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# デタッチ時に走ることを期待したスクリプト。
+# 「最初に一回」の処理に対しては冪等性を考慮し、記述すること。
+# author : kazuhito_m
+
+# サービス開始(二回目以降はリスタート)
+service sshd restart
+service tomcat6 restart
+service mysqld restart
+# 日に二度Tomcatを上げ直すため、cron起動
+service crond restart
+
+# mysqlに外から入れるよう設定(二回目以降も権限系だけはふり直す)
+mysql --user=root --connect-expired-password -e "SET PASSWORD FOR root@localhost=PASSWORD('kazuhito');"
+mysql --user=root --password=kazuhito -e 'CREATE DATABASE IF NOT EXISTS kazuhito;'
+mysql --user=root --password=kazuhito -e 'GRANT ALL PRIVILEGES ON *.* TO kazuhito@"%.%.%.%" IDENTIFIED BY "kazuhito";'
+mysql --user=root --password=kazuhito -e 'GRANT ALL PRIVILEGES ON *.* TO kazuhito@"localhost" IDENTIFIED BY "kazuhito";'
+
+# デーモン用にエンドレス実行の何かを置く
+tail -f /dev/null
