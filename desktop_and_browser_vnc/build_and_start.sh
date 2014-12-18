35a56e1ab0ea770a4fbac99b898a6f5cdd037703
kazuhito-m <kazuhito.sumpic@gmail.com>
2014-12-19 07:10:21 +0900

    VNCのポートが外から観測不能となっていたため、設定追加。

--- desktop_and_browser_vnc/build_and_start.sh
+++ desktop_and_browser_vnc/build_and_start.sh
@@ -11,4 +11,6 @@ CONTAINER_NAME="${BASE_NAME}_${CONTAINER_NO}"
 docker build -t ${IMAGE_NAME} .
 
 # container run
-docker run -d --name ${CONTAINER_NAME} -i -p ${CONTAINER_NO}0022:22 -p ${CONTAINER_NO}4444:4444 -t ${IMAGE_NAME}
+# FIXME:vncポートだけは「コンテナ番号での付け替え」を行わず。そのままの番号で外だし。
+#       （つまりは同一マシン内部に複数のVNCDockerが存在出来ないということ）
+docker run -d --name ${CONTAINER_NAME} -i -p ${CONTAINER_NO}0022:22 -p ${CONTAINER_NO}4444:4444 -p 5901:5901 -t ${IMAGE_NAME}
