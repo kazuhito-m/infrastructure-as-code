24fcade2f7c2bd16e23c8d35e60cc66cbadc6a2e
k-miura-biz <kazuhito.sumpic@gmail.com>
2015-03-20 19:38:45 +0900

    インストールスクリプトを追加。

---
+++ scripts/chef_install/chef_install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# 簡易な「サーバにChefを入れるまで」というスクリプトをまとめる。
+# (Chef以降はChef側にまかせる)
+#
+# ※極力楽するので、本当は良くないがyumに依存したり、いろいろ筋の悪いことをしているのは承知の上。
+#
+# 対象:CentOS7
+# user:root
+# 作業dir:任意
+#
+
+# TODO 必要とあらばここに環境の前準備(Proxyとか)
+
+# Ruby最新インストール(リポジトリ任せ)
+yum install -y ruby git
+
+# Chefインストール(ユーザはroot前提。一般ユーザならsudo付けて)
+curl http://www.opscode.com/chef/install.sh | bash
+
+# レポジトリの作成。
+git clone http://github.com/opscode/chef-repo.git
+
+# クックブックの作成。
+knife configure
