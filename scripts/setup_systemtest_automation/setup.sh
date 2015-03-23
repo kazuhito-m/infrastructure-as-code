08fdaea5aae4590ef89939b2ffcaaedc5dfb0d72
kazuhito-m <kazuhito.sumpic@gmail.com>
2015-03-24 03:24:02 +0900

    IntelliJ Idea のインストールスクリプトを追加。

--- scripts/setup_systemtest_automation/setup.sh
+++ scripts/setup_systemtest_automation/setup.sh
@@ -1,7 +1,17 @@
-#!/bin/bash
+#!/bin/bash -x
 
 # システムテスト自動化ワークショップ用、Ubunutのセットアップ用スクリプト。
 
 # Javaセットアップ
 sudo apt-get install openjdk-8-jdk -y
+
+# IntalliJ IDEA インストール
+rm -f ./ideaIC*.tar.gz
+wget http://download.jetbrains.com/idea/ideaIC-14.0.3.tar.gz
+gunzip ./ideaIC*.tar.gz
+tar xf ./ideaIC*.tar
+rm ./ideaIC*.tar
+rm -rf /opt/idea/
+sudo mv ./idea-IC* /opt/idea
+echo 'export PATH=/opt/idea/bin:$PATH' >> ~/.bashrc
 
