98b37697abe1cdb1a9c16c6f0d4e557a5703c6d6
kazuhito-m <kazuhito.sumpic@gmail.com>
2015-05-06 02:32:45 +0900

    Pacemakerの設定を流し込むコマンドを追加。

--- scripts/setup_pacemaker/chef-repo/site-cookbooks/custom-pacemaker/recipes/default.rb
+++ scripts/setup_pacemaker/chef-repo/site-cookbooks/custom-pacemaker/recipes/default.rb
@@ -39,3 +39,26 @@ service "corosync" do
   action [ :enable, :start ]
   supports :status => true, :restart => true, :reload => true
 end
+
+# corosyncの設定をコマンドで放り込む。
+bash "" do
+  code <<-EOH
+    pcs property set stonith-enabled=false
+    pcs property set no-quorum-policy=ignore
+    pcs resource create vip1 ocf:heartbeat:IPaddr2  nic="eth0" ip=172.17.1.20 cidr_netmask=24 iflabel= op monitor interval=5s
+    pcs resource create dgw ocf:heartbeat:Route  destination="0.0.0.0/0" gateway="172.17.1.254" op monitor interval=5s
+    pcs resource group add  service1 vip1 dgw
+    pcs resource create apache ocf:heartbeat:apache params configfile="/etc/httpd/conf/httpd.conf"  op monitor interval="5s"
+    pcs resource clone apache apache-clone
+    pcs constraint colocation  add service1 apache-clone
+    pcs resource create tomcat6 lsb:tomcat6 op monitor interval="5s"
+    pcs resource clone tomcat6 tomcat6-clone
+    pcs constraint colocation  add service1 apache-clone tomcat6-clone
+    pcs resource create vip2 ocf:heartbeat:IPaddr2  nic="eth0" ip=172.17.1.21 iflabel=1 cidr_netmask=24 op monitor interval=5s
+    pcs resource group add service2 vip2
+    pcs resource create mysql lsb:mysql  op monitor interval="5s"
+    pcs resource clone mysql mysql-clone
+    pcs constraint  colocation add service2 mysql-clone
+  EOH
+end 
+
