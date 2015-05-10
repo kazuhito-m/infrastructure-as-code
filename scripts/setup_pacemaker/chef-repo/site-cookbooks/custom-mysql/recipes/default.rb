c4d0baf4154f3d7e8e7265b9458d7c365fbd7ea6
kazuhito-m <kazuhito.sumpic@gmail.com>
2015-05-11 01:27:28 +0900

    mysqlのパッケージインストール部分だけ追加。

--- scripts/setup_pacemaker/chef-repo/site-cookbooks/custom-mysql/recipes/default.rb
+++ scripts/setup_pacemaker/chef-repo/site-cookbooks/custom-mysql/recipes/default.rb
@@ -2,7 +2,11 @@
 # Cookbook Name:: custom-mysql
 # Recipe:: default
 #
-# Copyright 2015, YOUR_COMPANY_NAME
+# Copyright 2015, kazuhito_n
 #
 # All rights reserved - Do Not Redistribute
 #
+
+package "mysql-server" do
+  action :install
+end 
