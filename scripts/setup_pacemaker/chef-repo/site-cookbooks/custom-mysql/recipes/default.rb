354bc98fa3c98ae9a967385eae50b054f041dd90
kazuhito-m <kazuhito.sumpic@gmail.com>
2015-05-13 03:54:20 +0900

    MySQLの起動時のエラーを解消するため、mysql_upgradeを叩くのと、その後当該設定を消して再起動するレシピを追記した。

--- scripts/setup_pacemaker/chef-repo/site-cookbooks/custom-mysql/recipes/default.rb
+++ scripts/setup_pacemaker/chef-repo/site-cookbooks/custom-mysql/recipes/default.rb
@@ -24,3 +24,27 @@ end
 service "mysqld" do 
   action [:enable, :start]
 end
+
+# pluginテーブルやら「最初に必要なテーブル郡」を作るため、mysql付属の特殊コマンドを使う。
+bash "mysql update" do
+  user "mysql"
+  group "mysql"
+  code "mysql_upgrade"
+end
+
+# 一時的に変えていた設定を削除し、再度起動。
+bash "restart mysqld by normal mode" do
+  code <<-EOH
+    cat /etc/my.cnf | sed 's/skip-grant-tables//g' > /etc/my.cnf.conv
+    diff /etc/my.cnf /etc/my.cnf.conv > /dev/null
+    res=$?
+    if [[ $res -eq 0 ]] ; then
+      rm /etc/my.cnf.conv
+    else
+      mv /etc/my.cnf.conv /etc/my.cnf
+      chmod 755 /etc/my.cnf
+      service mysqld restart
+    fi
+  EOH
+end
+
