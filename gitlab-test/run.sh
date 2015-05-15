f08a7057a82ec5735f7797835400f028ac13ad8c
kazuhito_m <kazuhito.sumpic@gmail.com>
2015-05-15 23:57:13 +0900

    gitlabwを実行出来るDockerコンテナを起動するスクリプトを追加。

---
+++ gitlab-test/run.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# gitlabを試験的にDocker内で立ち上げるスクリプト
+# 
+# ３つのDockerコンテナを使い、GitLabを立ち上げる。
+#
+# 配布元: https://github.com/sameersbn/docker-gitlab#introduction 
+# 
+# 前提
+#   作業はすべてrootで行う事。
+#   インターネットにつながっている事。
+#   dockerコマンドが使えること。
+#
+
+# 定数系
+
+SERVER_IP='192.168.1.1'
+
+
+# redis のサーバ立ち上げ
+docker run --name gitlab-redis -d sameersbn/redis:latest
+
+# gitlab用のMySQLの入れ物をhostmachine側に作成。
+mkdir -p /var/gitlab/{mysql,git}
+chmodsudo  777 -R /var/gitlab
+chcon -Rt svirt_sandbox_file_t /var/gitlab/mysql
+
+# gitlab用のMySQLサーバ立ち上げ
+docker run --name=mysql-gitlab -d \
+    --env='DB_NAME=gitlabhq_production' \
+      --env='DB_USER=gitlab' --env='DB_PASS=password' \
+        --volume=/var/gitlab/mysql:/var/lib/mysql \
+          sameersbn/mysql:latest
+    
+# 本丸、gitlabを立ち上げる
+docker run -d --name gitlab \
+    -p 10080:80 \
+      -e 'GITLAB_PORT=10080' \
+        -e "GITLAB_HOST=${SERVER_IP}" \
+          --link gitlab-mysql:mysql \
+            --link gitlab-redis:redisio \
+              sameersbn/gitlab:latest
+
+# 本来は、上記のコマンドに
+# --volume=/var/gitlab/git:/home/git/data \
+# というオプションがあったのだが、
+# "docker log gitlab" した結果、
+# 「mkdir で permission error 吐きまくる」
+# とわかったため、除いている。
+
+
+echo 'finished docker run for gitlab server wake up!'
+echo "please click http://${SERVER_IP}:10080/ after 5 - 10 min.'
+
