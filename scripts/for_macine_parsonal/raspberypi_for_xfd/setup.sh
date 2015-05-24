8ca171d4ae24f01786ce738fd951a1cf619d3f7a
kazuhito_m <kazuhito.sumpic@gmail.com>
2015-05-25 04:03:30 +0900

    Chef実行の記述を追加。

--- scripts/for_macine_parsonal/raspberypi_for_xfd/setup.sh
+++ scripts/for_macine_parsonal/raspberypi_for_xfd/setup.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
 
+# シェルとして書いているが…そのままじゃ繋がらないはずなので、一行一行コピペ前提で。
+
+# とりあえず、suじゃ。
+sudo su -
+
+# git で、このリポジトリを読み込む。
+git clone https://github.com/kazuhito-m/dockers.git
+cd ./dockers/scripts/for_macine_parsonal/raspberypi_for_xfd/chef-repo
+
+# chef実行。
+chef-solo -c ./solo.rb -j ./nodes/localhost.json
