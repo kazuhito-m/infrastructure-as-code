6b9df0d600cff3c416bdbd504376940e706542c6
kazuhito_m <kazuhito.sumpic@gmail.com>
2015-05-25 03:19:13 +0900

    raspbianの最新を取得するスクリプトを追加。

---
+++ scripts/setup_sdcard_for_raspbian/setup_sdcard_for_latest_raspbian_only.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# RaspabianのみをSDカードにコピーするスクリプト。
+# こちらを参考にダウンロード、SDカード焼付けを行う手順となっている。
+#   http://www.mztn.org/rpi/rpi39.html
+
+# OSイメージ系固定値
+download_url=http://downloads.raspberrypi.org/raspbian_latest
+
+# 自分マシンのSDのデバイス名(間違えると詰むので要確認！)
+sddev=/dev/mmcblk0
+
+# ダウンロード後のファイル名
+imagefile=raspbian.zip
+
+# ※必要な場合蘇らせる
+# sudo dd if=/dev/zero of=${sddev}
+
+# SDカードをパーティション設定＆フォーマット
+sudo fdisk ${sddev} # vfat、1パーティションで切る。
+sudo mkfs.vfat ${sddev}p1
+
+
+#ファイルをダウンロードして展開
+wget ${download_url} -O ${imagefile}
+unzip ${imagefile}
+imgfile=`ls *raspbian*.img`
+
+# SDカードに書き込む(かなり時間が掛かる)
+dd if=${imgfile} of=${sddev}
