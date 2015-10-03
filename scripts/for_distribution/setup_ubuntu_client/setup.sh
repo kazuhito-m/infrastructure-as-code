d2aa5a87f382326ad67b6dcd3acebf0ac52b56fd
kazuhito-m <kazuhito.sumpic@gmail.com>
2015-10-04 05:18:28 +0900

    肝心のChromeをインストールするコマンドが抜けてた。追加。

--- scripts/for_distribution/setup_ubuntu_client/setup.sh
+++ scripts/for_distribution/setup_ubuntu_client/setup.sh
@@ -36,6 +36,7 @@ sudo apt-get install atom
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 sudo apt-get update -y
+sudo apt-get insatall -y  google-chrome-stable
 
 # ドローイングソフト系
 sudo apt-get install -y gimp
