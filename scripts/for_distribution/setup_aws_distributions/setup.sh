8fb79ef7266daa56c2c4f700a50f08e7def71230
kazuhito_m <kazuhito.sumpic@gmail.com>
2015-10-17 02:27:09 +0900

    Gnomeが出てこないので、出てくる設定に変更。(なぜかUnityが出て来ないのはダメ)

--- scripts/for_distribution/setup_aws_distributions/setup.sh
+++ scripts/for_distribution/setup_aws_distributions/setup.sh
@@ -10,13 +10,12 @@ GIT_USER=kazuhito-m
 
 # ユーザを追加し、元のユーザを無効化する。
 
-sudo useradd -m ${MY_USER}
+sudo useradd -m -s '/bin/bash' ${MY_USER}
 sudo passwd ${MY_USER}
 # グループに追加(これはUbuntu固有？)
 for i in  sudo adm dialout cdrom floppy sudo audio dip video plugdev netdev ; do
   sudo gpasswd -a ${MY_USER} ${i}
 done
-sudo usermod -G sudo ${MY_USER}
 # 鍵コピー
 sudo mkdir /home/${MY_USER}/.ssh
 sudo cp /home/${DEFAULT_USER}/.ssh/authorized_keys /home/${MY_USER}/.ssh/authorized_keys
@@ -41,6 +40,7 @@ sudo chown ${MY_USER}:${MY_USER} /home/${MY_USER}/.netrc
 # ココからは「Ubuntuかつデスクトップ運用したい場合」のみの設定。
 # まずは「デスクトップを上げたいユーザでログインし、以下のコマンドを打つ。
 sudo apt-get update -y
+sudo apt-get upgrade -y
 sudo apt-get install -y gnome-core ubuntu-desktop vnc4server
 sudo service gdm start
 # 初回、vncserverパスワード決め
@@ -50,5 +50,11 @@ vncserver -kill :1
 # ~/.vnc/xstartup 書き換え
 mv ~/.vnc/xstartup ~/.vnc/xstartup.org
 grep -v '^x-.*' ~/.vnc/xstartup.org > ~/.vnc/xstartup
-echo 'exec gnome-session &' >> ~/.vnc/xstartup
-
+cat << _EOS_ >>  ~/.vnc/xstartup
+exec gnome-session &
+gnome-panel &
+gnome-settings-daemon &
+metacity &
+nautilus -n &
+gnome-terminal &
+_EOS_
