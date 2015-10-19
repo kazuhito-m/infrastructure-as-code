779fb1deaeb38485a2162849330ffa7901fe5a71
kazuhito-m <kazuhito.sumpic@gmail.com>
2015-10-20 08:30:20 +0900

    fabricのサンプルソースを追加。

---
+++ scripts/fabric_test/fabfile.py
@@ -0,0 +1,25 @@
+from fabric.api import local, run, sudo
+
+def hello():
+
+	# remote
+	run("uname -a")
+	sudo("head -n 5 /etc/sudoers")
+
+	# local
+	local("echo 'Hello World'")
+
+def japanize():
+	# change locale
+	sudo("apt-get install -y language-pack-ja", pty=False)
+	sudo("update-locale LANG=ja_JP.UTF-8")
+	# change timezone
+	sudo("mv /etc/localtime{,.org}")
+	sudo("ln -s /usr/share/zoneinfo/Asia/Tokyo /etc/localtime")
+
+def all_upgrade():
+	sudo("apt-get update", pty=False)
+#	sudo("do-release-upgrade", pty=False)
+	sudo("apt-get update", pty=False)
+	sudo("apt-get upgrade -y", pty=False)
+	sudo("apt-get dist-upgrade -y", pty=False)
