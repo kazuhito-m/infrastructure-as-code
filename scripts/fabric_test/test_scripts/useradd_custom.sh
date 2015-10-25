7d3c69e39982becd2deaefc40def67f2ff7a6f5c
kazuhito-m <kazuhito.sumpic@gmail.com>
2015-10-26 01:35:54 +0900

    sudo権限を与えるコードを追加。

--- scripts/fabric_test/test_scripts/useradd_custom.sh
+++ scripts/fabric_test/test_scripts/useradd_custom.sh
@@ -6,6 +6,9 @@ USER_NAME=${1}
 sudo useradd -m ${USER_NAME}
 echo "${USER_NAME}:test" | sudo chpasswd
 
+# sudo 与える
+sudo usermod -G sudo ${USER_NAME}
+
 # とあるファイルをコピー
 sudo cp -r ~/.vnc /home/${USER_NAME}/
 sudo chown -R ${USER_NAME}:${USER_NAME} /home/${USER_NAME}/.vnc
