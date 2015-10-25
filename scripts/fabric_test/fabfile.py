f0f8c5647ef073b195e01f16990dc7e8014698f0
kazuhito-m <kazuhito.sumpic@gmail.com>
2015-10-26 02:18:45 +0900

    パッケージで入れる手段を模索。

--- scripts/fabric_test/fabfile.py
+++ scripts/fabric_test/fabfile.py
@@ -29,9 +29,14 @@ def rename_home_template_dirs():
 	run("find ~/ -maxdepth 1 -type d  | LANG=C grep  -v '^[[:cntrl:][:print:]]*$' | xargs rm -rf")
 
 def install_msvsc():
-	run("wget https://az764295.vo.msecnd.net/public/0.9.1/VSCode-linux64.zip", pty=False)
-	run("unzip ./VSCode*.zip", pty=False)
-	run("rm ./VSCode*.zip")
-	sudo("mv ./VSCode* /usr/local/lib/")
-	sudo("ln -s /usr/local/lib/VSCode*/Code /usr/local/bin/VSCode")
+#	run("wget https://az764295.vo.msecnd.net/public/0.9.1/VSCode-linux64.zip", pty=False)
+#	run("unzip ./VSCode*.zip", pty=False)
+#	run("rm ./VSCode*.zip")
+#	sudo("mv ./VSCode* /usr/local/lib/")
+#	sudo("ln -s /usr/local/lib/VSCode*/Code /usr/local/bin/VSCode")
+	sudo("add-apt-repository -y ppa:ubuntu-desktop/ubuntu-make")
+	sudo("apt-get update -y")
+	sudo("apt-get install -y ubuntu-make")
+	run("umake -v web visual-studio-code")
+	# exec manualy command when miss install.
 	# extends in .vscode/extensions/ , configfile in .config/Code/User
