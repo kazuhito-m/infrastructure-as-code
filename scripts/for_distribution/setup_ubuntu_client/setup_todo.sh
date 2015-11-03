2f758699275fbdc7b2108bb6681d803f5ca41ce5
kazuhito-m <kazuhito.sumpic@gmail.com>
2015-11-03 21:44:52 +0900

    スクリプトからfabricの移植TODO管理。大体ぜんぶ移行出来た。

--- scripts/for_distribution/setup_ubuntu_client/setup_todo.sh
+++ scripts/for_distribution/setup_ubuntu_client/setup_todo.sh
@@ -4,24 +4,6 @@
 # すべての設定->外観->Launcherアイコンのサイズ を 32 に
 # すべての設定->ディスプレイ->拡大縮小 を 0.75 に
 
-# docker インストール
-sudo apt-get install docker
-
-# scala develop environment
-SCALA_VER='2.12.0-M2'
-SBT_VER='0.13.9'
-wget www.scala-lang.org/files/archive/scala-${SCALA_VER}.deb
-wget https://dl.bintray.com/sbt/debian/sbt-${SBT_VER}.deb
-sudo apt-get update
-sudo dpkg -i scala-${SCALA_VER}.deb sbt-${SBT_VER}.deb
-# conscript , giter8 (この技術は廃れるかもしれない。試験導入。)
-wget https://raw.github.com/n8han/conscript/master/setup.sh
-chmod 755 ./setup.sh
-./setup.sh
-rm ./setup.sh
-^/bin/cs n8han/giter8
-
-
 # -- add ---
 # special settings
 jenkins ALL=(ALL) NOPASSWD:ALL
