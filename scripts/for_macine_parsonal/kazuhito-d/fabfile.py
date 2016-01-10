e2d620802c955c327c77a2b5b402cb9bd6c5517f
kazuhito-m <kazuhito.sumpic@gmail.com>
2016-01-10 20:02:07 +0900

    IP固定化設定を追加。

--- scripts/for_macine_parsonal/kazuhito-d/fabfile.py
+++ scripts/for_macine_parsonal/kazuhito-d/fabfile.py
@@ -7,8 +7,16 @@ from fabric.api import local, run, sudo, put, env
 #
 def setup_all():
 	all_upgrade()
-	install_custom_tools()
+	# install_custom_tools()
 
 def install_custom_tools():
-	sudo("apt-get install -f -y ", pty=False)
+	sudo("apt-get install -f -y openssh-server", pty=False)
+
+def set_static_ip():
+	# DNS設定を中央集権に
+	sudo("apt-get install -f -y resolvconf", pty=False)
+	# IP固定設定。
+	# FIXME いろいろと決め打ちで書いてある。特に接続は"有線接続 1"という名前に固定してあるので、抽象化していきたい。
+	sudo("nmcli connection modify '有線接続 1' ipv4.method manual ipv4.addresses 192.168.1.130/24 ipv4.gateway 192.168.1.1 ipv4.dns 192.168.1.5") 
+
 
