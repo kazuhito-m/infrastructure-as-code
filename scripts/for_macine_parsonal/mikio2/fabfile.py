75983a881f3c4b70fb16072738deade39e6b2d56
kazuhito-m <kazuhito.sumpic@gmail.com>
2016-01-10 18:13:18 +0900

    SSHサーバにインストールする設定を追加。

--- scripts/for_macine_parsonal/mikio2/fabfile.py
+++ scripts/for_macine_parsonal/mikio2/fabfile.py
@@ -1,14 +1,9 @@
 #coding:utf-8
 from fabric.api import local, run, sudo, put, env
 
-SELF_MAIL_ADDRESS = "test@gmail.com"
-USER_NAME = "kazuhito-m"
-GIT_PASS = "xxxx"
 
 # 実行前に、以下のじゅんびが　必要です
 #
-# 上部の定数を個人設定の本当の値に置き換える
-# ./resource/.dropbox_uploader に値をいれたものを配置する
 #
 def setup_all():
 	all_upgrade()
@@ -22,5 +17,5 @@ def all_upgrade():
 	sudo("apt-get dist-upgrade -y", pty=False)
 
 def install_common_tools():
-	sudo("apt-get install -f -y tree indicator-multiload clipit", pty=False)
+	sudo("apt-get install -f -y tree indicator-multiload clipit openssh-server", pty=False)
 
