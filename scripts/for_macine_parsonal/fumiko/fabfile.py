4b1162403bfaf64bc279ced1fc3892eed13d401e
kazuhito-m <kazuhito.sumpic@gmail.com>
2016-02-01 20:51:49 +0900

    ネットワークの設定をするものを追加。

--- scripts/for_macine_parsonal/fumiko/fabfile.py
+++ scripts/for_macine_parsonal/fumiko/fabfile.py
@@ -5,6 +5,10 @@ from fabric.api import local, run, sudo, put, env
 #
 # 実行前の準備は、manual_operation.md 参照。
 
+# constant
+
+ETH_FILE="/etc/network/interfaces"
+
 def setup_all():
 	all_upgrade()
 	install_common_tools()
@@ -18,3 +22,19 @@ def all_upgrade():
 
 def install_common_tools():
 	sudo("apt-get install -f -y tree byobu", pty=False)
+
+def setup_network_settings():
+	TMP_PATH = '/tmp/interfaces'
+	backup_by_timestamp(ETH_FILE)
+	put("./resources/" + ETH_FILE , TMP_PATH)
+	sudo("cp %s %s" % (TMP_PATH,ETH_FILE))
+	sudo("ifdown eth0 && ifup eth0")
+
+# common function
+
+# ファイルを「末尾に年月日時分秒な名称」をつけてコピーする
+def backup_by_timestamp(filepath):
+	# おそらく設定ファイル系が多いと思うのでrootでやる。
+	sudo('cp ' + filepath + ' ' + filepath + '.`date +%Y%m%d%H%M%S`')	
+	
+
