7064a40a407489788f62a1cc3175048ee8750257
kazuhito-m <kazuhito.sumpic@gmail.com>
2016-02-01 18:25:14 +0900

    新しいKURO-NAS/X4(の中に入れる普通のPC)のAsCodeを追加。

---
+++ scripts/for_macine_parsonal/fumiko/fabfile.py
@@ -0,0 +1,23 @@
+#coding:utf-8
+from fabric.api import local, run, sudo, put, env
+
+# KURO-NAS/X4の跡地に入れた「普通のPC」fumikoのAsCodeしたものです。
+#
+# 実行前の準備は、manual_operation.md 参照。
+
+def setup_all():
+	all_upgrade()
+	install_common_tools()
+
+def all_upgrade():
+	sudo("apt-get update", pty=False)
+	# sudo("do-release-upgrade -d", pty=False)
+	sudo("apt-get update", pty=False)
+	sudo("apt-get upgrade -y", pty=False)
+	sudo("apt-get dist-upgrade -y", pty=False)
+
+def install_common_tools():
+	sudo("apt-get install -f -y tree indicator-multiload clipit openssh-server", pty=False)
+
+
+	
