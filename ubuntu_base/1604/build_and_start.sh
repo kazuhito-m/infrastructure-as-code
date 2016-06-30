25e469c7571da095d76e4496f2b3efc04e608346
kazuhito-m <kazuhito.sumpic@gmail.com>
2016-06-30 11:31:32 +0900

    最新型のUbuntuベースを作成

---
+++ ubuntu_base/1604/build_and_start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+IMAGE_NAME='ubuntu:1404base'
+
+CONTAINER_NO='5'
+CONTAINER_NAME="ubuntu1510_${CONTAINER_NO}"
+
+# image build
+docker build -t ${IMAGE_NAME} .
+
+# container run
+docker run -d --name ${CONTAINER_NAME} -i -p ${CONTAINER_NO}0022:22 -p ${CONTAINER_NO}8080:8080 -p ${CONTAINER_NO}3306:3306 -t ${IMAGE_NAME}
+
