08fc18f44f334679146263fc3529d042960bda50
kazuhito <kazuhito.sumpic@gmail.com>
2016-08-07 07:55:47 +0900

    但し書きを追加。

--- scripts/for_macine_parsonal/setup_chromebook_asusc300.sh
+++ scripts/for_macine_parsonal/setup_chromebook_asusc300.sh
@@ -7,4 +7,5 @@ sudo dd if=/dev/zero of=/home/swap0 bs=1M count=8192
 sudo chmod 600 /home/swap0 
 sudo mkswap /home/swap0 
 sudo swapon /home/swap0
-sudo echo '/home/swap0 swap swap defaults 0 0' >> /etc/fstab 
+# ここ、絶対こけるので、viで/etc/fstabを直接編集すること。
+sudo "echo '/home/swap0 swap swap defaults 0 0' >> /etc/fstab" 
