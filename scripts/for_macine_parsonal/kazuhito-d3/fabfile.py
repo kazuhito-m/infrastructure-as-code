406d734db8c7a298eb87594e69c0b922989dd747
kazuhito <kazuhito.sumpic@gmail.com>
2016-12-22 04:02:12 +0900

    微調整。fabfile.pyのインデントがおかしかったので修正

--- scripts/for_macine_parsonal/kazuhito-d3/fabfile.py
+++ scripts/for_macine_parsonal/kazuhito-d3/fabfile.py
@@ -21,8 +21,8 @@ def set_static_ip():
 	sudo("nmcli connection modify '有線接続 1' ipv4.method manual ipv4.addresses 192.168.1.133/24 ipv4.gateway 192.168.1.1 ipv4.dns 192.168.1.5 ipv4.dns-search local.sumpic.orz.hm")
 
 def install_display_driver():
-    run("curl http://us.download.nvidia.com/XFree86/Linux-x86_64/375.10/NVIDIA-Linux-x86_64-375.10.run > /tmp/NVIDIA.run")
-    run("chmod 755 /tmp/NVIDIA.run")
+	run("curl http://us.download.nvidia.com/XFree86/Linux-x86_64/375.10/NVIDIA-Linux-x86_64-375.10.run > /tmp/NVIDIA.run")
+	run("chmod 755 /tmp/NVIDIA.run")
 	sudo("service lightdm stop")
 	sudo("/tmp/NVIDIA.run")
 	sudo("service lightdm start")
