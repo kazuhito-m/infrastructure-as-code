616ad44b772ae53289828f7228c4bcbe44795cf8
kazuhito-m <kazuhito.sumpic@gmail.com>
2016-12-21 18:43:57 +0900

    example modify

--- scripts/fabric_test/setup_sample.sh
+++ scripts/fabric_test/setup_sample.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-fab  -H 54.178.170.79 -u ubuntu -i [鍵] japanize
-fab  -H 54.178.170.79 -u ubuntu --initial-password-prompt setup_all
+fab  -H 192.168.1.133 -u ubuntu -i [鍵] japanize
+fab  -H 192.168.1.133 -u ubuntu --initial-password-prompt setup_all
 
