56b532de44661d0546d0779f57a6235713830cf3
kazuhito-m <kazuhito.sumpic@gmail.com>
2018-02-24 00:03:30 +0900

    Terraformの基本ディレクティブ作成。VPC作成のサンプル追加。

--- terraform/basic-pattern/main.tf
+++ terraform/basic-pattern/main.tf
@@ -1,3 +1,13 @@
 provider "aws" {
     region = "ap-northeast-1"
 }
+
+resource "aws_vpc" "myVPC" {
+    cidr_block = "10.1.0.0/16"
+    instance_tenancy = "default"
+    enable_dns_support = "true"
+    enable_dns_hostnames = "false"
+    tags {
+      Name = "myVPC"
+    }
+}
