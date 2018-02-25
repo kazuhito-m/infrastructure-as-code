cd9e5911493de2fa5945b08e13a998ef0cad9ddc
kazuhito-m <kazuhito.sumpic@gmail.com>
2018-02-25 23:59:37 +0900

    実装。ちゃんとしたの埋める。

--- terraform/basic-pattern/main.tf
+++ terraform/basic-pattern/main.tf
@@ -1,13 +1,21 @@
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "region" {
+    default = "ap-northeast-1"
+}
+ 
 provider "aws" {
-    region = "ap-northeast-1"
+    access_key = "${var.aws_access_key}"
+    secret_key = "${var.aws_secret_key}"
+    region = "${var.region}"
 }
 
-resource "aws_vpc" "myVPC" {
-    cidr_block = "10.1.0.0/16"
+resource "aws_vpc" "vpc-develop" {
+    cidr_block = "10.0.0.0/16"
     instance_tenancy = "default"
     enable_dns_support = "true"
     enable_dns_hostnames = "false"
     tags {
-      Name = "myVPC"
+      Name = "vpc-peels-develop"
     }
 }
