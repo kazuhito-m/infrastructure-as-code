08911893e909ef734c27ca2108ae9bf515883482
kazuhito-m <kazuhito.sumpic@gmail.com>
2018-02-26 09:50:09 +0900

    Terraform実装。SecurityGroup追加。

--- terraform/basic-pattern/main.tf
+++ terraform/basic-pattern/main.tf
@@ -135,3 +135,22 @@ resource "aws_security_group" "ScgAp" {
   }
   tags { Name = "scg-ap" }
 }
+
+resource "aws_security_group" "ScgDb" {
+  name        = "scg-db"
+  description = "DB role security group."
+  vpc_id      = "${aws_vpc.VpcDevelop.id}"
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "6"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+  tags { Name = "scg-db" }
+}
