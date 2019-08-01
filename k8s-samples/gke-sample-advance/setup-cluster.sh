6e2594c9a177e968b772e38b1810b082831e8431
kazuhito-m <kazuhito.sumpic@gmail.com>
2019-08-01 22:02:42 +0900

    implement: DBを作成する部分をスクリプト化。

--- k8s-samples/gke-sample-advance/setup-cluster.sh
+++ k8s-samples/gke-sample-advance/setup-cluster.sh
@@ -7,13 +7,19 @@
 # gcloud auth login
 
 PROJECT_NAME=kfa-growi-on-gke
-CLUSTER_NAME=kazuhito-sample
+CLUSTER_NAME=kazuhito-sample-01
 
 REGION=asia-northeast1
 
 NETWORK_NAME=gke-cluster-network
 SUBNNET_NAME=gke-cluster-subnet 
 RANGE=10.0.0.0/16
+
+DB_INSTANCE_NAME=db-sample01
+DB_CPU=1
+DB_MEMORY=4GB
+DB_STORAGE_SIZE=10GB
+DB_STORAGE_TYPE=HDD #安いの
 
 gcloud config set compute/zone ${REGION}-a
 gcloud config set project ${PROJECT_NAME}
@@ -26,12 +32,25 @@ gcloud compute networks subnets create ${SUBNNET_NAME} \
     --range ${RANGE}
 gcloud compute firewall-rules create ${NETWORK_NAME}-rule01 --network ${NETWORK_NAME} --allow tcp:80,icmp
 
+# Create DB(CloudSQL)
+gcloud sql instances create ${DB_INSTANCE_NAME} \
+    --async \
+    --availability-type=zonal \
+    --database-version=POSTGRES_9_6 \
+    --region=${REGION} \
+    --cpu=${DB_CPU} \
+    --memory=${DB_MEMORY} \
+    --storage-size=${DB_STORAGE_SIZE} \
+    --storage-type=${DB_STORAGE_TYPE}
+
 # Create GKE Clusters
-# gcloud container clusters create ${CLUSTER_NAME} \
-#     --machine-type=n1-standard-1 \
-#     --num-nodes=3 \
-#     --enable-ip-alias \
-#     --cluster-ipv4-cidr=/16 \
-#     --services-ipv4-cidr=/22
-# gcloud container clusters get-credentials ${CLUSTER_NAME}
+gcloud container clusters create ${CLUSTER_NAME} \
+    --network=${NETWORK_NAME} \
+    --subnetwork=${SUBNNET_NAME} \
+    --machine-type=n1-standard-1 \
+    --num-nodes=3 \
+    --enable-ip-alias \
+    --cluster-ipv4-cidr=/16 \
+    --services-ipv4-cidr=/22
+gcloud container clusters get-credentials ${CLUSTER_NAME}
 
