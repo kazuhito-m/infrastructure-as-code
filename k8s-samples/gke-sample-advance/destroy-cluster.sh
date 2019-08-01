f8e7eb5fd14088ccccdc97d9c6aed121c16ec100
kazuhito-m <kazuhito.sumpic@gmail.com>
2019-08-02 01:03:33 +0900

    implement: VPN、DB、K8Sを消すスクリプトを作成。;

--- k8s-samples/gke-sample-advance/destroy-cluster.sh
+++ k8s-samples/gke-sample-advance/destroy-cluster.sh
@@ -7,8 +7,22 @@
 # gcloud auth login
 
 PROJECT_NAME=kfa-growi-on-gke
-CLUSTER_NAME=kazuhito-sample
+CLUSTER_NAME=kazuhito-sample-01
+
+REGION=asia-northeast1
+
+NETWORK_NAME=gke-cluster-network
+SUBNNET_NAME=gke-cluster-subnet 
+
+DB_INSTANCE_NAME=db-sample01
 
 gcloud container clusters delete ${CLUSTER_NAME}
 
 echo "削除完了しました。GCPのComputeEngineから、「クラスタに使われていたVMのディスク」を削除してください。"
+
+gcloud sql instances delete ${DB_INSTANCE_NAME}
+
+gcloud compute firewall-rules delete ${NETWORK_NAME}-rule01
+gcloud compute networks subnets delete ${SUBNNET_NAME} --region ${REGION}
+# コンソールから「VPCピアリングネットワーク」で属しているものをすべて削除してからでしか、以下を実行できない
+# gcloud compute networks delete ${NETWORK_NAME}
