db0c337342a242ebd46fc80c569c00388ed9470c
kazuhito-m <kazuhito.sumpic@gmail.com>
2020-11-30 01:51:57 +0900

    implement: 追加する拡張を追加。

--- scripts/fabric_ubuntu_standard/scripts/vscode-extensions.sh
+++ scripts/fabric_ubuntu_standard/scripts/vscode-extensions.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
+# standard
 code --install-extension coenraads.bracket-pair-colorizer
+code --install-extension esbenp.prettier-vscode
+
+# TypeScript
+code --install-extension steoates.autoimport
+code --install-extension mike-co.import-sorter
+code --install-extension stringham.move-ts
+
+# js/es
+code --install-extension dsznajder.es7-react-js-snippets
+
+# Docker
 code --install-extension ms-azuretools.vscode-docker
