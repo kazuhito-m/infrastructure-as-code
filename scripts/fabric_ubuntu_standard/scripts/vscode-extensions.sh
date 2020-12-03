74b2febe8fe9e9a057735750f504115cf47a86f1
kazuhito-m <kazuhito.sumpic@gmail.com>
2020-12-04 02:07:25 +0900

    implement: VSCodeの拡張を追加。

--- scripts/fabric_ubuntu_standard/scripts/vscode-extensions.sh
+++ scripts/fabric_ubuntu_standard/scripts/vscode-extensions.sh
@@ -5,6 +5,8 @@ code --install-extension coenraads.bracket-pair-colorizer
 code --install-extension esbenp.prettier-vscode
 code --install-extension oderwat.indent-rainbow
 code --install-extension mechatroner.rainbow-csv 
+code --install-extension streetsidesoftware.code-spell-checker
+code --install-extension ms-vsliveshare.vsliveshare
 
 # TypeScript
 code --install-extension steoates.autoimport
@@ -16,6 +18,7 @@ code --install-extension dsznajder.es7-react-js-snippets
 
 # C Sharp
 code --install-extension ms-dotnettools.csharp
+code --install-extension k--kato.docomment
 
 # Docker
 code --install-extension ms-azuretools.vscode-docker
