9d86ab79345d645ea224eb45230ba5a468348358
kazuhito_m <kazuhito.sumpic@gmail.com>
2022-07-18 18:07:30 +0900

    implent: 一通りの実装を完了。

--- scripts/for_macine_parsonal/fumiko/resources/usr/local/lib/maintenance/health_check_raid_array.sh
+++ scripts/for_macine_parsonal/fumiko/resources/usr/local/lib/maintenance/health_check_raid_array.sh
@@ -11,28 +11,40 @@
 # 定数
 SELF_HOST_NAME=$(hostname)
 SELF_HOST_DOMAIN_NAME=${SELF_HOST_NAME}.local.miu2.f5.si
-SELF_HOST_IP=$(host ${SELF_HOST_DOMAIN_NAME})
+SELF_HOST_IP=$(host ${SELF_HOST_DOMAIN_NAME} | sed 's/.*address //g')
 
-# 設定読込
-source ../../etc/maintenance/slack.conf
+# Path調整
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 
 # 自身スクリプトまで移動。
 SCRIPT_DIR=$(cd $(dirname $(readlink -f $0 || echo $0));pwd -P)
 cd ${SCRIPT_DIR}
 
+# 設定読込
+source ../../etc/maintenance/slack.conf
 
+# main part
 
-echo ${SLACK_WEBHOOK_URL}
+echo "${0} start at `date '+%Y/%m/%d %R'`"
 
+cat /proc/mdstat | grep 'blocks' | grep '\[UU\]'
+if [ $? -eq 0 ]; then
+    echo '異常無し。正常終了。'
+    echo "${0} end   at `date '+%Y/%m/%d %R'`"
+    exit 0
+fi
 
+## 異常時
 
+mdstat=$(/proc/mdstat)
 
 data=`cat <<_EOT_
 {
      "attachments": [
         {
 	        "mrkdwn_in": ["text"],
-            "color": "error",
+            "color": "danger",
+            "title": "MD Array Invalid Status",
             "pretext": "${SELF_HOST_NAME} のソフトウェアRAIDのArrayの状態異常です。",
             "fields": [
                 {
@@ -41,9 +53,9 @@ data=`cat <<_EOT_
                     "short": false
                 },
                 {
-                    "title": "ContainerTag/Version",
-                    "value": "${CI_BUILD_TAG}",
-                    "short": false
+                    "title": "Status Detail",
+                    "value": "cat /proc/mdadm said... \\\`\\\`\\\`${mdstat}\\\`\\\`\\\` ",
+                    "short": true
                 },
             ],
         }
@@ -52,3 +64,8 @@ data=`cat <<_EOT_
 _EOT_`
 
 ./slack_send.sh "${data}" ${SLACK_WEBHOOK_URL}
+
+echo '異常在り。異常終了。'
+echo "${0} end   at `date '+%Y/%m/%d %R'`"
+
+exit 1
