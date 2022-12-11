52c30cb13deceb80f10ebd5b7c40ea96dea83633
kazuhito_m <kazuhito.sumpic@gmail.com>
2022-12-12 01:19:16 +0900

    refactor: 変数を外だし化と、テスト用のスクリプトを実装。

---
+++ scripts/for_macine_parsonal/minoru/run_part_test.sh
@@ -0,0 +1,18 @@
+# テスト用のレシピのプロビジョニングを実行するスクリプト
+
+# 「このスクリプトがある場所」まで移動
+SCRIPT_DIR=$(cd $(dirname $(readlink -f $0 || echo $0));pwd -P)
+cd ${SCRIPT_DIR}
+
+# Ansbile実行
+export ANSIBLE_HOST_KEY_CHECKING=False  # known_hosts の判定を回避
+
+ansible-playbook \
+    part_test.yml \
+    -i ./hosts \
+    -u kazuhito \
+    --ask-pass \
+    --ask-become-pass \
+    -e 'ansible_python_interpreter=/usr/bin/python3'
+
+
